89801f53d4410b5457998411636dbb4e24160f5e
Ian Edwards <ian.dev.contact@gmail.com>
2022-03-01 20:55:48 +0000

    published express hello yuzu

--- app.mjs
+++ app.mjs
@@ -1,21 +1,14 @@
 #!/usr/bin/env node
-import * as http from 'http'
-import * as url from 'url'
+import express from 'express'
 
+const app = express();
 const hostname = process.env.YUZU_HOSTNAME
 const port = process.env.YUZU_PORT
 
-import { helloTools } from './tools.mjs'
+app.get('/yuzu', (req, res) => {
+  res.send('hello yuzu')
+})
 
-http.createServer((req, res) => {
-  const query = url.parse(req.url, true).query;
-  const seed = query?.seed ?? Math.random();
-  helloTools()
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({
-    seed: "workin?"
-  }));
-}).listen(port, hostname, () => {
-    console.log(`yuzu listening http://${hostname}:${port}`);
-});
+app.listen(port, hostname, () => {
+  console.log(`yuzu listening http://${hostname}:${port}/yuzu`)
+})
